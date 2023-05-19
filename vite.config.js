363a82e64ba25ff80ec0fe8d5ee5a8fea0faa929
Gonzotsh <gonzalo.andres.88@gmail.com>
2023-05-19 13:57:25 -0400

    ajuste de directorio

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/https://gonzotsh.github.io/consultaPokeAPI' ,
+    base: '/consultaPokeAPI' ,
     build: {
         rollupOptions: {
             input: {
